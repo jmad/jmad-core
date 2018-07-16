82db133776d8dea298f8e8121dd9ccc523db1d1a
mihostet <michi.hostettler@cern.ch>
2018-07-16 19:35:58 +0200

    another try with updated binaries - use intel binary for linux

--- update-mad-binaries.sh
+++ update-mad-binaries.sh
@@ -4,9 +4,13 @@ MADX_URL_BASE=http://madx.web.cern.ch/madx/releases/last-rel
 
 for os in {linux,osx,win}; do
     ospath="$os"
+    compiler="gnu"
     if [ "$os" == 'osx' ]; then
         ospath='macosx'
     fi
+    if [ "$os" == 'linux' ]; then
+        compiler="intel" # numerical discrepancies for ... gnu
+    fi
     rm "src/java/cern/accsoft/steering/jmad/bin/$os/$destbin"
-    wget "$MADX_URL_BASE/madx-${ospath}64-gnu" -O "src/java/cern/accsoft/steering/jmad/bin/$os/madx64"
+    wget "$MADX_URL_BASE/madx-${ospath}64-$compiler" -O "src/java/cern/accsoft/steering/jmad/bin/$os/madx64"
 done
